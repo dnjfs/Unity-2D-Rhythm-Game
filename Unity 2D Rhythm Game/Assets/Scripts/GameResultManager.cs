17240ccbe3eb3e0f9f4538aca4db189707ee72ee
정호진 <48848466+dnjfs@users.noreply.github.com>
2019-11-19 02:57:14 +0900

    랭크 보여주기 및 게임 시작 화면으로 돌아가기

--- Unity 2D Rhythm Game/Assets/Scripts/GameResultManager.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/GameResultManager.cs
@@ -2,23 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameResultManager : MonoBehaviour
 {
     public Text musicTitleUI;
     public Text scoreUI;
     public Text maxComboUI;
+    public Image RankUI;
 
     void Start()
     {
         musicTitleUI.text = PlayerInformation.musicTitle;
-        scoreUI.text = "" + PlayerInformation.score;
-        maxComboUI.text = "" + PlayerInformation.maxCombo;
+        scoreUI.text = "점수: " + (int)PlayerInformation.score;
+        maxComboUI.text = "최대 콤보: " + PlayerInformation.maxCombo;
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + PlayerInformation.selectedMusic);
+        StringReader reader = new StringReader(textAsset.text);
+
+        reader.ReadLine();
+        reader.ReadLine();
+        string beatInformation = reader.ReadLine();
+        int scoreS = Convert.ToInt32(beatInformation.Split(' ')[3]);
+        int scoreA = Convert.ToInt32(beatInformation.Split(' ')[4]);
+        int scoreB = Convert.ToInt32(beatInformation.Split(' ')[5]);
+
+        if (PlayerInformation.score >= scoreS)
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank S");
+        else if (PlayerInformation.score >= scoreA)
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank A");
+        else if (PlayerInformation.score >= scoreB)
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank B");
+        else
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Replay()
     {
-        
+        SceneManager.LoadScene("SongSelectScene");
     }
 }
